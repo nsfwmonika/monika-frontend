c7bdcba9e92a157e51e35442578d1a605098b285
lucas-monika <lucas@nsfwmonika.ai>
2024-12-21 17:07:21 +0800

    fix:add video

--- components/home/HomeIndex.tsx
+++ components/home/HomeIndex.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/home/Hero";
 import Mapping from "@/components/home/Mapping";
 import Roadmap from "@/components/home/Roadmap";
 import Reward from "@/components/home/Reward";
+import VideoModel from "@/components/home/VideoModel";
 
 export default async function HomeIndex() {
 
@@ -11,6 +12,7 @@ export default async function HomeIndex() {
       <Hero id="About"/>
       <Mapping id="Creation"/>
       <BrowseImages id="Browse" />
+      <VideoModel></VideoModel>
       <Reward id="Reward"></Reward>
       <Roadmap id="Roadmap"></Roadmap>
     </>
