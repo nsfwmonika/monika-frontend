3ccf18b1dd1b042ad36e70a7ea9b5f667d00c05e
lucas-monika <lucas@nsfwmonika.ai>
2025-01-02 18:35:31 +0800

    fix:add images

--- config/images/face-swap.ts
+++ config/images/face-swap.ts
@@ -27,21 +27,44 @@ export const faceSwapImages: ImageItem[] = [
         child: ["/img/face-swap/4-1.png", "/img/face-swap/4-2.png"],
         show: true
     },{
-        result: "/img/face-swap/4-1.png",
+        result: "/img/face-swap/5-1.png",
         original: "/img/face-swap/4-2.png",
-        prompt:"By utilizing face swapping technology, we replaced her facial features with those of another girl, achieving face replacement while keeping the rest of the image unchanged. The composition centers her, emphasizing her calm, introspective expression. The soft, natural light enhances the serene atmosphere, while the muted color palette contrasts with the subtle gold accents, creating a harmonious balance.",
+        prompt:"With Face Swap technology, we replaced her facial features with those of another girl, realizing face replacement while keeping the rest of the image unchanged. Arbitrarily change your dress and scene to easily present a different style of you.",
+        child: ["/img/face-swap/5-2.png", "/img/face-swap/5-5.png"],
+        show: true
+    },{
+        result: "/img/face-swap/6-1.png",
+        original: "/img/face-swap/3-2.png",
+        prompt:"With Face Swap technology, we replaced her facial features with those of another girl, realizing face replacement while keeping the rest of the image unchanged. Arbitrarily change your dress and scene to easily present a different style of you.",
+        child: ["/img/face-swap/6-2.png", "/img/face-swap/6-3.png"],
+        show: true
+    },{
+        result: "/img/face-swap/7-1.png",
+        original: "/img/face-swap/4-2.png",
+        prompt:"With Face Swap technology, we replaced her facial features with those of another girl, realizing face replacement while keeping the rest of the image unchanged. Arbitrarily change your dress and scene to easily present a different style of you.",
+        child: ["/img/face-swap/7-2.png", "/img/face-swap/7-3.png"],
+        show: true
+    },
+    {
+        result: "/img/face-swap/7-1.png",
+        original: "/img/face-swap/4-2.png",
+        prompt:"With Face Swap technology, we replaced her facial features with those of another girl, realizing face replacement while keeping the rest of the image unchanged. Arbitrarily change your dress and scene to easily present a different style of you.",
         child: ["/img/face-swap/4-1.png", "/img/face-swap/4-2.png"],
         show: false
-    },{
-        result: "/img/face-swap/3-1.png",
-        original: "/img/face-swap/3-2.png",
-        prompt:"By utilizing face swapping technology, we replaced her facial features with those of another girl, achieving face replacement while keeping the rest of the image unchanged. The composition centers her, emphasizing her calm, introspective expression. The soft, natural light enhances the serene atmosphere, while the muted color palette contrasts with the subtle gold accents, creating a harmonious balance.",
-        child: ["/img/face-swap/3-1.png", "/img/face-swap/3-2.png"],
-        show: false
-    },{
-        result: "/img/face-swap/4-1.png",
+    },
+    
+    {
+        result: "/img/face-swap/7-1.png",
         original: "/img/face-swap/4-2.png",
-        prompt:"By utilizing face swapping technology, we replaced her facial features with those of another girl, achieving face replacement while keeping the rest of the image unchanged. The composition centers her, emphasizing her calm, introspective expression. The soft, natural light enhances the serene atmosphere, while the muted color palette contrasts with the subtle gold accents, creating a harmonious balance.",
+        prompt:"With Face Swap technology, we replaced her facial features with those of another girl, realizing face replacement while keeping the rest of the image unchanged. Arbitrarily change your dress and scene to easily present a different style of you.",
+        child: ["/img/face-swap/4-1.png", "/img/face-swap/4-2.png"],
+        show: false
+    },
+    
+    {
+        result: "/img/face-swap/7-1.png",
+        original: "/img/face-swap/4-2.png",
+        prompt:"With Face Swap technology, we replaced her facial features with those of another girl, realizing face replacement while keeping the rest of the image unchanged. Arbitrarily change your dress and scene to easily present a different style of you.",
         child: ["/img/face-swap/4-1.png", "/img/face-swap/4-2.png"],
         show: false
     },
